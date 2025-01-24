24679976cde7a2d31fbbb41d2c362b39331a68f6
boromirl <burdiyanov@gmail.com>
2025-01-25 01:13:56 +0700

    initial structure

---
+++ library/my_lib.cc
@@ -0,0 +1,7 @@
+#include "my_lib.h"
+
+void func_1(std::string str){}
+
+int func_2(std::string str){return 0;}
+
+bool func_3(std::string str){return 0;}
---
+++ library/my_lib.h
@@ -0,0 +1,10 @@
+#ifndef MY_LIB_H
+#define MY_LIB_H
+
+#include <string>
+
+void func_1(std::string str);
+int func_2(std::string str);
+bool func_3(std::string str);
+
+#endif
