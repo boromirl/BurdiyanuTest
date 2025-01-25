a7bc5eedd332120d73e50e58849c1e5b14950d11
boromirl <burdiyanov@gmail.com>
2025-01-25 12:04:30 +0700

    google style

--- library/my_lib.cc
+++ library/my_lib.cc
@@ -1,22 +1,23 @@
 #include "my_lib.h"
 
-void func_1(std::string &str){
-    std::sort(str.begin(), str.end(), std::greater<char>());
+void func_1(std::string &str) {
+  std::sort(str.rbegin(), str.rend());
 
-    std::string temp;
-    temp.reserve(str.length() + str.length() / 2);
+  std::string temp;
+  temp.reserve(str.length() + str.length() / 2);
 
-    for(int i = 0; i < str.length(); i++){
-        if(i % 2 == 1){ // из-за начала с 0 (изменить?)
-            temp.append("KB");
-        } else{
-            temp.push_back(str[i]);
-        }
+  for (int i = 0; i < str.length(); i++) {
+    if (i % 2 == 1) {  // из-за начала с 0
+      temp.append("KB");
+    } else {
+      temp.push_back(str[i]);
     }
+  }
 
-    str = temp;
+  str = temp;
+  int t = 5;
 }
 
-int func_2(std::string str){return 0;}
+int func_2(std::string str) { return 0; }
 
-bool func_3(std::string str){return 0;}
+bool func_3(std::string str) { return 0; }
--- library/my_lib.h
+++ library/my_lib.h
@@ -1,8 +1,9 @@
 #ifndef MY_LIB_H
 #define MY_LIB_H
 
-#include <string>
 #include <algorithm>
+#include <string>
+
 
 void func_1(std::string &str);
 int func_2(std::string str);
